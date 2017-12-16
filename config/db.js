1024fafb9ff85d5f3ce4ac31345a9e1bc116d529
Matheus Galdino <matheus.galdino@hotmail.com>
2017-12-16 00:13:11 -0200

    Conexão com mlab

--- config/db.js
+++ config/db.js
@@ -1,3 +1,5 @@
-module.exports = {
-    url: 'mongodb://isatheus:isatheus@ds141796.mlab.com:41796/messagio'
-}
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://isatheus:isatheus@ds141796.mlab.com:41796/messagio', { 
+    useMongoClient: true 
+})
