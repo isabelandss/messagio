06bb673573dff6ed1f5a80659edf0e6704908ce2
Matheus Galdino <matheus.galdino@hotmail.com>
2017-12-16 00:13:24 -0200

    Estrutura inicial do server

---
+++ app.js
@@ -0,0 +1,16 @@
+const   express     = require('express'),
+        bodyParser  = require('body-parser')
+
+require('./config/db');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+    console.log(`Estamos rodando na porta ${port}`);
+})
